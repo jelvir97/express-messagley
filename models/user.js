b82b042b91fd55fbd4d985e9abe342abfbf215f3
Juan Elvir <jelvir97@gmail.com>
2023-11-16 18:25:55 -0700

    User.get(username)

--- models/user.js
+++ models/user.js
@@ -68,7 +68,13 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const result = await db.query(`SELECT username, first_name, last_name, phone, join_at, last_login_at
+                                    FROM users
+                                    WHERE username = $1`,[username])
+    const user = result.rows[0]
+    return user
+   }
 
   /** Return messages from this user.
    *
