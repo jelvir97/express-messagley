86d5eac2c2383d19dab43c5c1ffe2c82be1ba1f0
Juan Elvir <jelvir97@gmail.com>
2023-11-20 18:42:38 -0700

    set req.user = payload.user

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,7 @@ function authenticateJWT(req, res, next) {
   try {
     const tokenFromBody = req.body._token;
     const payload = jwt.verify(tokenFromBody, SECRET_KEY);
-    req.user = payload; // create a current user
+    req.user = payload.user; // create a current user
     return next();
   } catch (err) {
     return next();
